f46d1e7037d1a6876492312623bd4f21c259375e
lienhart.michaël <lienhartm8@gmail.com>
2024-10-22 15:52:31 +0200

    CurriculumVitae-website

--- script.js
+++ script.js
@@ -94,10 +94,6 @@ function contact() {
                                                     + "<input type='submit' value='Envoyer'>"
                                                     + "</form>"
                                                     + "</section>";
-
-    (function(){
-        emailjs.init("UH2k-274wOM005RNe");
-    })();
     
     window.onload = function() {
             document.getElementById('contact-form').addEventListener('submit', function(event) {
