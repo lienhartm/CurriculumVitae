807772f2154e48c96a4d50b513af94d41b2ed35c
lienhart.michaël <lienhartm8@gmail.com>
2024-10-23 13:49:24 +0200

    CurriculumVitae-website

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ function home() {
     document.getElementById('main').style.backgroundColor = 'white';
     document.getElementById('main').innerHTML =     "<section class='home'>"
                                                     + "<p>Bienvenue sur mon site de présentation !</p>"
-                                                    + "<br>"
-                                                    + "<p>J'ai été fasciné par l'informatique, dès lors que j'ai pu apercevoir des informaticiens travailleret se divertir...</p>"
-                                                    + "<br>"
-                                                    + "<p>J'ai toujours pu entretenir ma part de connaissance tout au long de ma formation, même quand je m'évertuai dans d'autre proffession.</p>"
-                                                    + "<br>"
-                                                    + "<p>Documentations, ouvrages, tutoriels </p>"
+                                                    + "<br />"
+                                                    + "<p>Je suis étudiant en licence professionelle Métiers de l'informatique : conception, développement et tests de logiciels.</p>"
+                                                    + "<br />"
+                                                    + "<p>Vous pouvez me m'écrire à cette adresse : lienhartm8@gmail.com</p>"
+                                                    + "<br />"
+                                                    + "<p>Vous pouvez aussi télécharger mon <a href='./document/CV-LIENHART_Michaël.pdf' download class='telecharger'>curriculum vitae</a>.</p>"
                                                     + "</section>";
 
 }
@@ -21,15 +21,9 @@ function research() {
 
     document.getElementById('main').style.backgroundColor = 'white';
     document.getElementById('main').innerHTML =     "<section class='research'>"
-                                                    + "<ul>"
-                                                    + "<li>Organisations: Scrum, méthode Agile</li>"
-                                                    + "<li>Gestion de projet: Jira / Confluence / Slack / Jitsi</li>"
-                                                    + "<li>Conception: Cahier des charge, maquettage, UML</li>"
-                                                    + "<li>Language: HTML / CSS / JS / PHP / SQL / JAVA</li>"
-                                                    + "<li>Framework: React.js / Next.js / Svelt.js / Wordpress / Symphony"
-                                                    + "<li>Logiciel: Linux / Git / Docker</li>"
-                                                    + "<li>Certification: PIX, SQL, JAVA (notions), Développement coté client</li>"
-                                                    + "</ul>"
+                                                    + "<div>"
+                                                    + "<p>Je suis à la recherche d'un stage de fin d'études qui me permettra de mettre en pratique mes compétences en développement logiciel et de contribuer à des projets concrets.<br /><br />Je suis particulièrement intéressé par les domaines spécifiques comme le développement web, les tests logiciels, et je souhaite acquérir une expérience enrichissante dans un environnement dynamique et stimulant.</p>"
+                                                    + "</div>"
                                                     + "</section>";
 
 }
@@ -39,13 +33,26 @@ function cv() {
     document.getElementById('main').style.backgroundColor = 'white';
     document.getElementById('main').innerHTML =     "<section class='cv'>"
                                                     + "<h2>Comptétences</h2>"
-                                                    + "<p></p>"
+                                                    + "<div>"
+                                                    + "<ul>"
+                                                    + "<li>Organisations: Scrum, méthode Agile</li>"
+                                                    + "<li>Gestion de projet: Jira / Confluence / Slack / Jitsi</li>"
+                                                    + "<li>Conception: Cahier des charge, maquettage, UML</li>"
+                                                    + "<li>Language: HTML / CSS / JS / PHP / SQL / JAVA</li>"
+                                                    + "<li>Framework: React.js / Next.js / Svelt.js / Wordpress / Symphony"
+                                                    + "<li>Logiciel: Linux / Git / Docker</li>"
+                                                    + "<li>Certification: PIX, SQL, JAVA (notions), Développement coté client</li>"
+                                                    + "</ul>"
+                                                    + "</div>"
                                                     + "<h2>Expériences</h2>"
+                                                    + "<div>"
                                                     + "<ul>"
                                                     + "<li>Stagiaire - ELAN Formation - Brunsstat-Didenheim</li>"
                                                     + "<li>Informaticien Polyvalent - INFORMATIQUE+ IAE34 - Montpellier</li>"
                                                     + "</ul>"
+                                                    + "</div>"
                                                     + "<h2>Formations</h2>"
+                                                    + "<div>"
                                                     + "<ul>"
                                                     + "<li>2023 - en cours Licence Professionnelle - Métiers de l'informatique : conception, développement et tests de logiciels – UHA4.0 Université de Haute Alsace</li>"
                                                     + "<li>2022 / 2023 - Certificat Professionel de Webmestre Junior - CNAM GRAND EST - Auditeur</li>"
@@ -54,6 +61,7 @@ function cv() {
                                                     + "<li>2001 / 2003 - Bac Pro Equipements et installations électrique - Pullversheim</li>"
                                                     + "<li>1999 / 2001 - CAP / BEP électrotechnique - Blaise Pascal Colmar</li>"
                                                     + "</ul>"
+                                                    + "</div>"
                                                     + "</section>";
 
 }
@@ -62,15 +70,17 @@ function project() {
 
     document.getElementById('main').style.backgroundColor = 'white';
     document.getElementById('main').innerHTML =     "<section class='project'>"
+                                                    + "<div>"
                                                     + "<ul>"
-                                                    + "<li>Fil Rouge Projet 401 - Basketball</li>"
-                                                    + "<li>Fil Rouge Projet 402 - Mind</li>"
-                                                    + "<li>Fil Rouge Projet 403 - BlocNote</li>"
-                                                    + "<li>Design & Concept - Application de gestion de devis / facture</li>"
+                                                    + "<li>Basketball - Fil Rouge Projet 401</li>"
+                                                    + "<li>Mind - Fil Rouge Projet 402</li>"
+                                                    + "<li>BlocNote - Fil Rouge Projet 403</li>"
+                                                    + "<li>Design & Concept - Application de gestion de clients, devis et facture</li>"
                                                     + "<li>HoplaCup - Application de gestion de tournoi de water-polo</li>"
                                                     + "<li>Saime - Application de managemant d'entreprise</li>"
-                                                    + "<li>Caritas - Audit</li>"
+                                                    + "<li>Caritas - Audit et statistiques</li>"
                                                     + "</ul>"
+                                                    + "</div>"
                                                     + "</section>";
 
 }
@@ -79,29 +89,28 @@ function contact() {
 
     document.getElementById('main').style.backgroundColor = 'white';
     document.getElementById('main').innerHTML =     "<section class='contact'>"
-                                                    + "<p>Vous pouvez me m'écrire à cette adresse : lienhartm8@gmail.com</p>"
-                                                    + "<br />"
-                                                    + "<p>Vous pouvez aussi télécharger mon <a href='./document/CV-LIENHART_Michaël.pdf' download class='telecharger'>curriculum vitae</a>.</p>"
-                                                    + "<br />"
+                                                    + "<div>"
                                                     + "<form id='form'>"
                                                     + "<div class='field'>"
-                                                    + "<label for='from_name'>from_name</label>"
-                                                    + "<input type='text' name='from_name' id='from_name'>"
+                                                    + "<label for='from_name'>Nom:</label>"
+                                                    + "<input type='text' name='user_name' id='from_name'>"
                                                     + "</div>"
                                                     + "<div class='field'>"
-                                                    + "<label for='user_name'>user_name</label>"
-                                                    + "<input type='text' name='user_name' id='user_name'>"
+                                                    + "<label for='user_name'>Objet:</label>"
+                                                    + "<input type='text' name='objet' id='user_name'>"
                                                     + "</div>"
                                                     + "<div class='field'>"
-                                                    + "<label for='message'>message</label>"
+                                                    + "<label for='message'>Message:</label>"
+                                                    + "<br />"
                                                     + "<textarea type='text' name='message' id='message' rows='4' cols='50'></textarea>"
                                                     + "</div>"
                                                     + "<div class='field'>"
-                                                    + "<label for='user_email'>user_email</label>"
+                                                    + "<label for='user_email'>Email:</label>"
                                                     + "<input type='text' name='user_email' id='user_email'>"
                                                     + "</div>"
                                                     + "<input type='submit' id='button' value='Send Email' >"
                                                     + "</form>"
+                                                    + "</div>"
                                                     + "</section>";
 
         const btn = document.getElementById('button');
